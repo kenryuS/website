151885b64a4ea8d0cd64f868eea58e521834866c
kenryuS <kenryushibata@gmail.com>
2023-05-08 22:05:16 -0400

    Introduced mobile navigation bar and added post

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,11 @@ import styles from '../styles/footer.module.css'
 export default function Footer() {
     return (
         <div className={styles.cont}>
-            <div></div>
+            <footer>
+            <div className={styles.spacerTop}></div>
             <p>Created by kenryuS with Next.js</p>
+            <div className={styles.spacerBottom}></div>
+            </footer>
         </div>
     )
 }
--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,17 @@
 import Navbar from "./navigatorbar";
+import NavbarMobile from "./navigatorbar-mobile";
 import Footer from "./footer";
 import Sidebar from './hamburger-menu';
 
 export default function Layout({children}) {
     return (
         <div id="outer-container">
-            <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+            {/* <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} /> */}
             <div id="page-wrap">
                 <Navbar/>
                 <main>{children}</main>
                 <Footer/>
+                <NavbarMobile/>
             </div>
         </div>
     )
